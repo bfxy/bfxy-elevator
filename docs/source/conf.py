4f3ab858e907ffeba2b875171ccd73455b8d8674
bfxy <al.werehere@gmail.com>
2015-08-12 22:47:43 +0300

    Theme change

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -51,7 +51,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Elevator User Guide'
+project = u'Elevator'
 copyright = u'2015, Vitali Lobanov'
 author = u'Vitali Lobanov'
 
@@ -113,7 +113,7 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
